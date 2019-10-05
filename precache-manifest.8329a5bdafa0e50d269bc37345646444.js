3e701311aef81213ae074308764525b2b3d0fe36
KuaqSon <qson.ng@gmail.com>
2019-10-05 23:15:08 +0700

    Updates

---
+++ precache-manifest.8329a5bdafa0e50d269bc37345646444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6dfc70866546d0c529c65532b53871b",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "52484fa4385a086c5d3f",
+    "url": "/resume/static/css/main.16c2a4d7.chunk.css"
+  },
+  {
+    "revision": "5665230e10ef12eb1e09",
+    "url": "/resume/static/js/2.1907a53a.chunk.js"
+  },
+  {
+    "revision": "52484fa4385a086c5d3f",
+    "url": "/resume/static/js/main.0abcb9b2.chunk.js"
+  },
+  {
+    "revision": "0f17f0b7f1e3c6b1f94f",
+    "url": "/resume/static/js/runtime-main.d774290e.js"
+  },
+  {
+    "revision": "3f0cba28b3406f8249e1fe2a92013edf",
+    "url": "/resume/static/media/avatar-1.3f0cba28.jpg"
+  }
+]);
