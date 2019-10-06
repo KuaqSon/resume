952de855e2cb6430e6dcd68288a59e51882e1964
KuaqSon <qson.ng@gmail.com>
2019-10-06 19:05:03 +0700

    Removed unuse component

--- src/App.js
+++ src/App.js
@@ -14,32 +14,10 @@ import HomeImage from "./components/HomeImage";
 //Theme
 import { customTheme } from "./theme";
 
-// import useWindowSize from "react-use/lib/useWindowSize";
-// import Confetti from "react-confetti";
-
 function App() {
-  // const { width, height } = useWindowSize();
-
   return (
     <Grommet plain theme={customTheme}>
       <Header />
-      {/* <Confetti
-        numberOfPieces={40}
-        initialVelocityY={100}
-        width={width - 32}
-        height={height}
-        drawShape={ctx => {
-          ctx.beginPath();
-          for (let i = 0; i < 22; i++) {
-            const angle = 0.35 * i;
-            const x = (0.2 + 1.5 * angle) * Math.cos(angle);
-            const y = (0.2 + 1.5 * angle) * Math.sin(angle);
-            ctx.lineTo(x, y);
-          }
-          ctx.stroke();
-          ctx.closePath();
-        }}
-      /> */}
       <HomeImage />
       <Box
         width={{ max: "1500px" }}
