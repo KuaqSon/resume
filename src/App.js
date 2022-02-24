8066f77022399d6fbddddf6d95c9bcaf4597540e
kuaqson <qson.ng@gmail.com>
2022-02-24 16:36:30 +0700

    Updated February 2022

--- src/App.js
+++ src/App.js
@@ -32,11 +32,11 @@ const content = [
         skills: [
           {
             main: "Programming Languages",
-            desc: "C#, Javascript, TypeScript, HTML, CSS (SCSS).",
+            desc: "Python, Javascript, TypeScript, C#, HTML, CSS (SCSS).",
           },
           {
             main: "Frameworks & Libraries",
-            desc: "ASP .NET & ASP .NETCORE, NodeJS, ExpressJs, JQuery, ReactJS, Angular",
+            desc: "Django, NodeJS, ExpressJs, ASP .NET & ASP .NETCORE, JQuery, ReactJS, Angular",
           },
         ],
       },
@@ -45,13 +45,11 @@ const content = [
         skills: [
           {
             main: "COMMUNICATION",
-            desc:
-              "Through the process of learning at university, I have built the necessary communication skills for working flexibly.",
+            desc: "Through the process of learning at university, I have built the necessary communication skills for working flexibly.",
           },
           {
             main: "TEAM WORK",
-            desc:
-              "Be able to control and supervise activities of the group. Knowing how to motivate the team to achieve the best performance on work.",
+            desc: "Be able to control and supervise activities of the group. Knowing how to motivate the team to achieve the best performance on work.",
           },
         ],
       },
@@ -61,12 +59,63 @@ const content = [
     section: "EXPERIENCE AND PROJECTS",
     subs: [
       {
+        title: "CAREER DISCOVERY & DEVELOPMENT PLATFORM",
+        skills: [
+          {
+            main: "Main activities",
+            desc: "Build a system to aggregate and provide a large number of quality jobs. Build a system for collecting, reviewing, and filtering millions of job listings. Developing a web-application that will connect consultants and job seekers so that they can increase success rates and discover and build a good career path.",
+          },
+          {
+            main: "Python, Django, ReactJS, Data Crawler, Postgres",
+            desc: "",
+          },
+        ],
+      },
+      {
+        title: "ONLINE TUTORING SYSTEM",
+        skills: [
+          {
+            main: "Main activities",
+            desc: "Build a system to help students and teachers connect and arrange online learning sessions. Clusters of specific functions such as set up an available class schedule, booking classes, rating teachers, browsing teacher profiles, finding the right classes.",
+          },
+          {
+            main: "NodeJS, ExpressJS, ReactJS, Ant Design, Sequelize ORM, Postgres",
+            desc: "",
+          },
+        ],
+      },
+      {
+        title: "PAYMENT PORTAL FOR A BLOCKCHAIN-BASED PLATFORM",
+        skills: [
+          {
+            main: "Main activities",
+            desc: "Build a web-app to serve the needs of fundraising, buying, tracking, referral for crypto projects.",
+          },
+          {
+            main: "NodeJS, NestJS, ReactJS, Chakra UI, TypeORM, Postgres",
+            desc: "",
+          },
+        ],
+      },
+      {
+        title: "NEWS AGGREGATOR SYSTEM",
+        skills: [
+          {
+            main: "Main activities",
+            desc: "Build micro services systems to serve projects on aggregating news from many newspapers and magazines. Publish news to video on social platforms. Build a website to publish news aggregated into topics, collections, personalized.",
+          },
+          {
+            main: "NodeJS, ExpressJS, NestJS, HTML, SCSS, JQUERY, MySQL",
+            desc: "",
+          },
+        ],
+      },
+      {
         title: "SCHOOL MANAGEMENT PORTAL",
         skills: [
           {
             main: "Main activities",
-            desc:
-              "Build and scale frontend components for large system to manage student, trainer, lesson, certificate,...",
+            desc: "Build and scale frontend components for large system to manage student, trainer, lesson, certificate,...",
           },
           {
             main: "HTML, SCSS, JQUERY",
@@ -92,8 +141,7 @@ const content = [
         skills: [
           {
             main: "Main activities",
-            desc:
-              "Build backend api to connect and integrate with third party. Build single-page app for user can registration an internet service.",
+            desc: "Build backend api to connect and integrate with third party. Build single-page app for user can registration an internet service.",
           },
           {
             main: "ASP .NET, Angular 6",
@@ -106,8 +154,7 @@ const content = [
         skills: [
           {
             main: "Main activities",
-            desc:
-              "Design and build a backend service connect with Lazada and shoppe api also build an internal api crud the inventory. Build front end single page app have good UX for user.",
+            desc: "Design and build a backend service connect with Lazada and shoppe api also build an internal api crud the inventory. Build front end single page app have good UX for user.",
           },
           {
             main: "NodeJS, ExpressJS, ReactJs, Redux-Saga, Ant Design",
@@ -144,7 +191,7 @@ const Header = () => {
           <div className="contact">
             <div className="contact_item">
               <div className="contact_title">Address.</div>
-              <div className="contact_cont">90/46A Duong Ba Trac Street, Ward 2, District 8, Ho Chi Minh City</div>
+              <div className="contact_cont">104/9 Nhat Chi Mai st, Tan Binh, Ho Chi Minh City</div>
             </div>
             <div className="contact_item">
               <div className="contact_title">Phone.</div>
@@ -157,7 +204,7 @@ const Header = () => {
             <div className="contact_item">
               <div className="contact_title">Facebook.</div>
               <div className="contact_cont">
-                <a target="_blank" href="https://www.facebook.com/quangson.nguyen.399">
+                <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/quangson.nguyen.399">
                   quangson.nguyen.399
                 </a>
               </div>
@@ -165,7 +212,7 @@ const Header = () => {
             <div className="contact_item">
               <div className="contact_title">Github.</div>
               <div className="contact_cont">
-                <a target="_blank" href="https://github.com/KuaqSon">
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/KuaqSon">
                   github.com/KuaqSon
                 </a>
               </div>
@@ -173,8 +220,8 @@ const Header = () => {
             <div className="contact_item">
               <div className="contact_title">Personal website.</div>
               <div className="contact_cont">
-                <a target="_blank" href="https://nqson.xyz/">
-                  nqson.xyz
+                <a target="_blank" rel="noopener noreferrer" href="https://nqson.com/">
+                  nqson.com
                 </a>
               </div>
             </div>
@@ -186,7 +233,7 @@ const Header = () => {
 };
 
 const Footer = () => {
-  return <div className="container footer">Nguyễn Quang Sơn - Updated August 2020</div>;
+  return <div className="container footer">Nguyễn Quang Sơn - Updated February 2022</div>;
 };
 
 const Main = () => {
